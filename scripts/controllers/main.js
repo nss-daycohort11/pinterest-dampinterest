5bfc2478017f81d4d1befa0a272b39292d28752a
Anderson Cook <andersoncook@Andersons-MacBook-Pro.local>
2015-12-08 11:29:15 -0600

    updated gruntfile, commented out facebook oauth

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -16,13 +16,17 @@ angular.module('pinterestApp')
         ref.set({picUrl:"",
           bio: "blank",
           name: "no name",
-          email: $scope.email});
+          email: $scope.email,
+          pins: false});
       }).catch(function(error) {
         $scope.error = error;
       });
     };
 
     $scope.loginUser = function() {
+      $scope.message = null;
+      $scope.error = null;
+      console.log('running');
       var ref = new Firebase("https://dampinterest.firebaseio.com");
           ref.authWithPassword({
             email    : $scope.email,
@@ -30,7 +34,7 @@ angular.module('pinterestApp')
           }, function(error, authData) {
             if (error) {
               console.log("Login Failed!", error);
-              $scope.error = "Login Failed: " + error + "."
+              $scope.error = "Login Failed: " + error + ".";
             } else {
               console.log("Authenticated successfully with payload:", authData);
               $scope.message = "Authenticated successfully: " + authData + ".";
@@ -38,15 +42,37 @@ angular.module('pinterestApp')
           });    
     };
 
-      $scope.loginFbook = function() {
-        // login with Facebook
-        console.log("maybeworked?");
-       
-        Auth.$authWithOAuthPopup("facebook").then(function(authData) {
-          console.log("Logged in as:", authData.uid);
-        }).catch(function(error) {
-          console.log("Authentication failed:", error);
-        });
-      };
-    }
+
+      // $scope.loginFbook = function() {
+      //   console.log("maybeworked?");
+      //   var ref = new Firebase("https://dampinterest.firebaseio.com");
+      //   var authData = ref.getAuth();
+      //   console.log("authdata: ", authData);
+
+      //   if(authData === null) {
+      //     ref.authWithOAuthPopup("facebook", function(error, authData) {
+      //       if (error) {
+      //         console.log("Login Failed!", error);
+      //       } else {
+      //         console.log("authenticated", authData);
+      //         ref.child("users/" + authData.uid).set({
+      //           picUrl: "",
+      //           bio: "blank",
+      //           name: "no name",
+      //           email: authData
+      //         });
+      //   } else {
+      //     ref.child("users/" + authData.uid).set(authData);
+      //   }
+      //     });
+      //   }
+      // };
+
+    //     // Auth.$authWithOAuthPopup("facebook").then(function(authData) {
+    //     //   console.log("Logged in as:", authData.uid);
+    //     // }).catch(function(error) {
+    //     //   console.log("Authentication failed:", error);
+    //     // });
+
+  }
 ]);
