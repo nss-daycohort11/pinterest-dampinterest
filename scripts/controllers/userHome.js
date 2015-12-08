bbf1ab68121d4e82b60e1f78a9b3612808dc60c3
hollisd09 <hollisd09@gmail.com>
2015-12-08 15:36:13 -0600

    Well holy bickets, the modal is working y'all '

--- scripts/controllers/userHome.js
+++ scripts/controllers/userHome.js
@@ -1,7 +1,20 @@
 angular
 	.module('pinterestApp')
-  .controller('UserHomeCtrl', ["$scope", "Auth",
-  	function($scope, Auth) {
-  		console.log("what's the buzz");
-  	}
+  .controller('UserHomeCtrl', ["$scope", "Auth", "$firebaseArray", "getUid",
+  	function($scope, Auth, $firebaseArray, getUid) {
+  		var uid = getUid.getUid();
+  		var ref = new Firebase("https://dampinterest.firebaseio.com/pins/");
+
+  		$scope.pins = $firebaseArray(ref);
+  		$scope.createdPin = {};
+
+  		$scope.newPin = function() {
+  			$scope.pins.$add({
+  				picURL: $scope.createdPin.picURL,
+  				picInfo: $scope.createdPin.picInfo,
+  				creator: uid
+  			})
+  				console.log("uid", uid);
+  		}
+		}
   ]);
