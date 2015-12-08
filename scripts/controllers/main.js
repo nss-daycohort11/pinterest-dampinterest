6b1747295b8de649806ae70a8848c2d23757d936
Anderson Cook <andersoncook@Andersons-MacBook-Pro.local>
2015-12-08 10:09:03 -0600

    initial commit

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -12,10 +12,32 @@ angular.module('pinterestApp')
         password: $scope.password
       }).then(function(userData) {
         $scope.message = "User created with uid: " + userData.uid;
+        var ref = new Firebase("https://dampinterest.firebaseio.com/users/" + userData.uid);
+        ref.set({picUrl:"",
+          bio: "blank",
+          name: "no name",
+          email: $scope.email});
       }).catch(function(error) {
         $scope.error = error;
       });
     };
+
+    $scope.loginUser = function() {
+      var ref = new Firebase("https://dampinterest.firebaseio.com");
+          ref.authWithPassword({
+            email    : $scope.email,
+            password : $scope.password
+          }, function(error, authData) {
+            if (error) {
+              console.log("Login Failed!", error);
+              $scope.error = "Login Failed: " + error + "."
+            } else {
+              console.log("Authenticated successfully with payload:", authData);
+              $scope.message = "Authenticated successfully: " + authData + ".";
+            }
+          });    
+    };
+
       $scope.loginFbook = function() {
         // login with Facebook
         console.log("maybeworked?");
