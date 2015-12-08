8154234f85f201f8e622eebc96a2db3860364d67
hollisd09 <hollisd09@gmail.com>
2015-12-08 14:32:03 -0600

    Things look beautiful, and pictures are populating

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,7 +1,7 @@
 angular.module('pinterestApp')
-  .controller('MainCtrl', ["$scope", "Auth", "$location",
+  .controller('MainCtrl', ["$scope", "Auth", "$location", "getUid",
   
-  function($scope, Auth, $location) {
+  function($scope, Auth, $location, getUid) {
     $scope.createUser = function() {
       $scope.message = null;
       $scope.error = null;
@@ -12,6 +12,8 @@ angular.module('pinterestApp')
       }).then(function(userData) {
         console.log("things", userData)
         $scope.message = "User created with uid: " + userData.uid;
+        getUid.addUid(userData.uid);
+        console.log("getUid.addUid(userData.uid)", getUid.addUid(userData.uid));
         $location.path("/userHome");
         $scope.logInNav = true;
         var ref = new Firebase("https://dampinterest.firebaseio.com/users/" + userData.uid);
@@ -40,6 +42,7 @@ angular.module('pinterestApp')
             } else {
               console.log("Authenticated successfully with payload:", authData);
               $scope.message = "Authenticated successfully: " + authData + ".";
+              getUid.addUid(authData.uid);
               $location.path("/userHome");
               $scope.logInNav = true;
               $scope.$apply();
