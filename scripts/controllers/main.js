b82d2533d4ff3846e6bc24fab0be569419bd6147
mdhays <mdhays1995@gmail.com>
2015-12-08 11:45:41 -0600

    Added Anderson's code to main.js.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,33 +1,45 @@
 angular.module('pinterestApp')
-  .controller('MainCtrl', ["$scope", "Auth", "$location", 
+  .controller('MainCtrl', ["$scope", "Auth", "$location",
   
   function($scope, Auth, $location) {
     $scope.createUser = function() {
       $scope.message = null;
       $scope.error = null;
       console.log('running');
-
       Auth.$createUser({
         email: $scope.email,
         password: $scope.password
       }).then(function(userData) {
-        console.log("things", userData)
         $scope.message = "User created with uid: " + userData.uid;
-        $location.path("/userHome");
+        var ref = new Firebase("https://dampinterest.firebaseio.com/users/" + userData.uid);
+        ref.set({picUrl:"",
+          bio: "blank",
+          name: "no name",
+          email: $scope.email,
+          pins: false});
+          $location.path("/userHome");
       }).catch(function(error) {
         $scope.error = error;
       });
     };
-      $scope.loginFbook = function() {
-        // login with Facebook
-        console.log("maybeworked?");
-       
-        Auth.$authWithOAuthPopup("facebook").then(function(authData) {
-          console.log("Logged in as:", authData.uid);
-          $location.path("/userHome");
-        }).catch(function(error) {
-          console.log("Authentication failed:", error);
-        });
-      };
-    }
+    $scope.loginUser = function() {
+      $scope.message = null;
+      $scope.error = null;
+      console.log('running');
+      $location.path("/userHome");
+      var ref = new Firebase("https://dampinterest.firebaseio.com");
+          ref.authWithPassword({
+            email    : $scope.email,
+            password : $scope.password
+          }, function(error, authData) {
+            if (error) {
+              console.log("Login Failed!", error);
+              $scope.error = "Login Failed: " + error + ".";
+            } else {
+              console.log("Authenticated successfully with payload:", authData);
+              $scope.message = "Authenticated successfully: " + authData + ".";
+            }
+          });    
+    };
+  }
 ]);
