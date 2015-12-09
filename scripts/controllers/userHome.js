0b7aff2bcae91e7cf5db9e5f553e8a8fbb4befc4
hollisd09 <hollisd09@gmail.com>
2015-12-09 10:21:58 -0600

    Made the things pretty :) And added moreModal

--- scripts/controllers/userHome.js
+++ scripts/controllers/userHome.js
@@ -4,17 +4,34 @@ angular
   	function($scope, Auth, $firebaseArray, getUid) {
   		var uid = getUid.getUid();
   		var ref = new Firebase("https://dampinterest.firebaseio.com/pins/");
+      var boardRef = new Firebase("https://dampinterest.firebaseio.com/boards");
 
   		$scope.pins = $firebaseArray(ref);
   		$scope.createdPin = {};
+      $scope.boards = $firebaseArray(boardRef);
+
+      $scope.$watch(function() {
+
+      })
 
   		$scope.newPin = function() {
   			$scope.pins.$add({
   				picURL: $scope.createdPin.picURL,
   				picInfo: $scope.createdPin.picInfo,
-  				creator: uid
+  				creator: uid,
+          board: $scope.selectedBoard.name
   			})
   				console.log("uid", uid);
+          $scope.createdPin.picURL = null;
+          $scope.createdPin.picInfo = null;
   		}
+
+      $scope.newBoard = function() {
+        $scope.boards.$add({
+          name: $scope.createdBoard.name
+        })
+        $scope.createdBoard.name = null;
 		}
+  }
+  
   ]);
